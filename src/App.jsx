a2c8fcedbb5c0294bc8f0f01f43b42fde2e0cb86
oktaydemirhas <oktaydemirhas@gmail.com>
2025-05-11 14:45:13 +0300

    tailwind kuruldu

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p className="">app </p>
-    </>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <h1 className="text-5xl font-extrabold text-white mb-8">
+        Tailwind Çalışıyor 🎯
+      </h1>
+      <button className="bg-white text-pink-500 font-semibold py-2 px-6 rounded-lg shadow-lg hover:bg-pink-100 transition">
+        Butona Bas
+      </button>
+    </div>
   );
 }
 
